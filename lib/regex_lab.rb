58cd4f6b360234d944d9c7b05499ab3bb5e765b3
Tara Meyerink <tarajmeyerink@gmail.com>
2018-06-03 18:25:08 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !! word.match(/^[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/\bun\w{1,}ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  return text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !! text.match(/^[A-Z].{1,}[?.!]$/)
 end
 
 def valid_phone_number?(phone)
-
+  !!phone.match(/\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
 end
